b527abc8321a5dcbb10d04ba86a6b27f9234862f
Rustem Galeev <r.galeev116@gmail.com>
2023-10-13 17:32:45 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container>
+    <v-row class='ma-3 justify-center text-h5 text-center'>Сформировать лот для розыгрыша</v-row>
+    <v-sheet width='auto' class='mx-3'>
+      <v-form fast-fail @submit.prevent>
+        <v-text-field
+            v-model='description'
+            label='Описание для администратора'
+            :rules='descriptionRules'
+        ></v-text-field>
+
+        <v-text-field
+            v-model='winnerDescription'
+            label='Описание для победителя'
+            :rules='winnerDescriptionRules'
+        ></v-text-field>
+
+        <v-btn type='submit' block class='mt-2' @click='postNewLot'>Сформировать</v-btn>
+      </v-form>
+    </v-sheet>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      api_url: process.env.VUE_APP_API_URL + '/api/lots',
+      description: '',
+      descriptionRules: [
+        value => {
+          if (value?.length > 0) return true
+
+          return 'Введите описание для администратора'
+        },
+      ],
+      winnerDescription: '',
+      winnerDescriptionRules: [
+        value => {
+          if (value?.length > 0) return true
+
+          return 'Введите описание для победителя'
+        },
+      ],
+    }
+  },
+  methods: {
+    postNewLot() {
+      axios.post(this.api_url, {
+        description: this.description,
+        winnerDescription: this.winnerDescription
+      })
+          .then(response => {
+            console.log(response)
+            window.close();
+          })
+          .catch(error => console.error(error))
+    },
+  }
+}
+</script>
+
+<style>
+</style>
