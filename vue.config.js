d684650746080558ca71b0f3932880d993c9ddbe
Rustem Galeev <r.galeev116@gmail.com>
2023-10-19 14:09:08 +0300

    deploy on github pages

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 const {defineConfig} = require('@vue/cli-service')
+const {resolve} = require('path');
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
         port: 3000
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/prize-drawer/' :
+        '/',
+    outputDir: resolve(__dirname, "./docs"),
+    assetsDir: './'
 })
